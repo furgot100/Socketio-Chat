f3b38945b063e181c1d97535ea875612b2046722
Francis Tsang <frtsang40@gmail.com>
2021-05-06 10:02:55 -0700

    users can create channels and join

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 //We'll store our online users here
 let onlineUsers = {};
-io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+let channels = {"General" : []};
+
 
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
@@ -19,6 +17,15 @@ app.get('/', (req, res) => {
   res.render('index.handlebars');
 })
 
+io.on("connection", (socket) => {
+  // Make sure to send the users to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers);
+})
+
+
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
 })
+
+
+
